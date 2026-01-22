93791bc1a7ef50ba710165d7cf124f96c014b8f9
nullhq <taboutsafredy@gmail.com>
2026-01-22 05:36:57 +0100

    chore: normalisation des ops de deploiement

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # app.py
-
+import os
 from flask import Flask, send_from_directory
 from src.routes.api import api
 
@@ -11,4 +11,5 @@ def index():
     return send_from_directory('static', 'index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port, debug=True)
