69b1172eef2bbed5bc82c24402b4e84ead6dc690
nullhq <taboutsafredy@gmail.com>
2026-01-22 04:48:57 +0100

    feat: index.html now exposed by the server

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 # app.py
 
-from flask import Flask
+from flask import Flask, send_from_directory
 from src.routes.api import api
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', static_url_path='')
 app.register_blueprint(api, url_prefix='/api')
+
+@app.route('/')
+def index():
+    return send_from_directory('static', 'index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
