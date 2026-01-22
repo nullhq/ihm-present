f8401a72c1672e504963bd485a3ca6c00af81b7a
nullhq <taboutsafredy@gmail.com>
2026-01-22 06:05:49 +0100

    chore: add render configuration

--- app.py
+++ app.py
@@ -5,11 +5,12 @@ from src.routes.api import api
 
 app = Flask(__name__, static_folder='static', static_url_path='')
 app.register_blueprint(api, url_prefix='/api')
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 # 16 MB limit
 
 @app.route('/')
 def index():
     return send_from_directory('static', 'index.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
